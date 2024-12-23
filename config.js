3b8fcbd59b1ead094fb49363612e68fdd6516972
Herexlnr <zerooption04@gmail.com>
2024-12-23 23:52:18 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "HVoXTbDT#HNq3mHk8SIb9zxfb95pkBzMlOGgmxKIhSBLmVKHeE1A",
+    SESSION_ID: process.env.SESSION_ID || "jVwAFQ6a#eJztNNodyhMLo2HgDY14Y7uymhBWMnl3K9AqPEgGqZw",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     READ_MESSAGE: process.env.READ_MESSAGE || "true", // Added auto-read configuration
     MODE: process.env.MODE || "public",
